c434b5672eb4f6e0fc3c689f8f31a96f6d24aff2
naziyasumra04 <girlofficial297@gmail.com>
2025-07-23 13:39:17 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:weather_app/presentation/screens/weather_page.dart';
+import 'business_logic/bloc/weather_bloc.dart';
+import 'data/repositories/weather_repository.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(); //
   runApp(const MyApp());
 }
 
@@ -11,8 +18,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Weather App',
-      // home:
+      home: BlocProvider(
+        create: (_) => WeatherBloc(WeatherRepository()),
+        child: const WeatherPage(),
+      ),
     );
   }
 }
